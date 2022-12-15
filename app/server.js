37b6e38730e731fdb4d4c738a5c05c3cfa49996b
KenDC1984 <ken.decrop@amista.be>
2022-12-15 18:35:25 +0100

    created server with no auth, working when deployed on cf

--- app/server.js
+++ app/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const env = require('cfenv');
 
 app.get('/runjob', function (req, res) {
     res.send('Job succesfully executed');
 });
 
-const port = process.env.port || 3000;
+//Not working => const port = process.env.port || 3000;
+const appenv = env.getAppEnv();
+const port = appenv.port || 3000;
 app.listen(port, function(){
     console.log('listening...');
 });
